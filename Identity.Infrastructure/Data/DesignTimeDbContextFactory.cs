ad7ace7f4bc7efaec0a650bd2f6f4e53c13f9d2d
BeshoyGrace <beshoy@velents.com>
2025-01-01 14:51:12 +0200

    Identity Services

--- Identity.Infrastructure/Data/DesignTimeDbContextFactory.cs
+++ Identity.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace Identity.Infrastructure.Data
 {
-    internal class DesignTimeDbContextFactory
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<IdentityContext>
     {
+        public IdentityContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<IdentityContext>();
+
+            // Use the connection string configured for your Azure SQL Database
+            optionsBuilder.UseSqlServer("Data Source=;Initial Catalog=; User ID=;Password=;Integrated Security=False;Trust Server Certificate=True;Encrypt=False");
+
+            return new IdentityContext(optionsBuilder.Options, null);
+        }
     }
 }
--- Identity.Infrastructure/Data/IdentityContext.cs
+++ Identity.Infrastructure/Data/IdentityContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,17 @@ using System.Threading.Tasks;
 
 namespace Identity.Infrastructure.Data
 {
-    public class IdentityContext
+    public class IdentityContext : IdentityDbContext
     {
+        private readonly IHttpContextAccessor _context;
+
+        public IdentityContext(DbContextOptions<IdentityContext> options, IHttpContextAccessor context) : base(options)
+        {
+            _context = context;
+        }
+        public IdentityContext()
+        {
+
+        }
     }
 }
