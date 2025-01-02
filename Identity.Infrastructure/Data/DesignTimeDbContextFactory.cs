32f6a52b673c484164a58fc720e90e0cb446494c
Khaled Oweas <khaled.oweas@yahoo.com>
2025-01-02 12:08:53 +0200

    Formatting file  .

--- Identity.Infrastructure/Data/DesignTimeDbContextFactory.cs
+++ Identity.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Identity.Infrastructure.Data
 {
@@ -12,9 +7,8 @@ namespace Identity.Infrastructure.Data
     {
         public IdentityContext CreateDbContext(string[] args)
         {
-            var optionsBuilder = new DbContextOptionsBuilder<IdentityContext>();
+            var optionsBuilder = new DbContextOptionsBuilder<IdentityContext>(); 
 
-            // Use the connection string configured for your Azure SQL Database
             optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Jada30_Identity; User ID=sa;Password=P@ssw0rd;Integrated Security=False;Trust Server Certificate=True;Encrypt=False");
 
             return new IdentityContext(optionsBuilder.Options, null);
