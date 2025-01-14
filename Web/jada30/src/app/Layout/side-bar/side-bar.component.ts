4a017edee8b8f2dc608ae08948b6a9b57e5ce316
Khaled Oweas <khaled.oweas@yahoo.com>
2025-01-14 12:28:40 +0200

    save

--- Web/jada30/src/app/Layout/side-bar/side-bar.component.ts
+++ Web/jada30/src/app/Layout/side-bar/side-bar.component.ts
@@ -1,11 +1,10 @@
-import { NgClass } from "@angular/common";
-import { Component, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+import { Component } from "@angular/core";
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: "app-side-bar",
   standalone: true,
-  imports: [RouterLink, NgClass, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: "./side-bar.component.html",
   styleUrl: "./side-bar.component.css"
 })
