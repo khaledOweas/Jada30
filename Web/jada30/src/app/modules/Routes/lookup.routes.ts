ff68bd52f96413bba60a3049ed0fa1106bae36b5
Khaled Oweas <khaled.oweas@yahoo.com>
2025-01-22 14:26:55 +0200

    Fix Layout And Router Issue .

--- Web/jada30/src/app/modules/Routes/lookup.routes.ts
+++ Web/jada30/src/app/modules/Routes/lookup.routes.ts
@@ -1,11 +1,4 @@
 import { Routes } from "@angular/router";
-import { LayoutComponent } from "src/app/_metronic/layout/layout.component";
 import { LookupListComponent } from "../lookup/lookup-list/lookup-list.component";
 
-export const LOOKUP_ROUTES: Routes = [
-  {
-    path: "",
-    component: LayoutComponent,
-    children: [{ path: "lookup-list/:code", component: LookupListComponent }]
-  }
-];
+export const LOOKUP_ROUTES: Routes = [{ path: "lookup-list/:code", component: LookupListComponent }];
--- Web/jada30/src/app/modules/Routes/user.routes.ts
+++ Web/jada30/src/app/modules/Routes/user.routes.ts
@@ -1,19 +1,12 @@
 import { Routes } from "@angular/router";
-import { LayoutComponent } from "src/app/_metronic/layout/layout.component";
 import { UserListComponent } from "../user/user-list/user-list.component";
 import { UserCreateComponent } from "../user/user-create/user-create.component";
 import { UserUpdateComponent } from "../user/user-update/user-update.component";
 import { RoleListComponent } from "../user/role-list/role-list.component";
 
 export const USER_ROUTES: Routes = [
-  {
-    path: "",
-    component: LayoutComponent,
-    children: [
-      { path: "user-list", component: UserListComponent },
-      { path: "user-create", component: UserCreateComponent },
-      { path: "user-update/:id", component: UserUpdateComponent },
-      { path: "role-list", component: RoleListComponent }
-    ]
-  }
+  { path: "user-list", component: UserListComponent },
+  { path: "user-create", component: UserCreateComponent },
+  { path: "user-update/:id", component: UserUpdateComponent },
+  { path: "role-list", component: RoleListComponent }
 ];
